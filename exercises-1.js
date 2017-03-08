03371138711570831e93d65adfb9c5f6aa929379
Erick G <lmterickg@cox.net>
2017-03-08 15:46:24 -0800

    final commit

--- exercises-1.js
+++ exercises-1.js
@@ -5,7 +5,7 @@ var add=function(x,y){
 	return x+y
 }
 function sum(x,y){
-	return(x+y)
+	return x + y
 }
 
 
@@ -13,10 +13,10 @@ function sum(x,y){
 // Q2
 // Write a function named `avg` that takes 3 parameters
 // and returns the average of those 3 numbers.
-var avg=function(x,y,z){
-	return (x+y+z)/3
+function avg(x,y,z){
+	return (x + y + z) / 3
 }
-function avg(a,b,c)
+
 
 
 
@@ -24,9 +24,10 @@ function avg(a,b,c)
 // Write a function called `getLength` that takes one
 // parameter (a string) and returns the length
 
-function getLenght(a){
+function getLength(a){
 	return a.length
 }
+	
 
 
 // Q4
@@ -34,7 +35,13 @@ function getLenght(a){
 // two parameters and returns `true` if the second
 // parameter is greater than the first. Otherwise
 // the function should return `false`.
-function
+function greaterThan(a , b){
+	if (a < b) {
+		return true
+	} else {
+		return false
+	}
+}
 
 
 
@@ -43,10 +50,9 @@ function
 // single parameter and returns a string that
 // is formated like "Hello, Name!" where *Name*
 // is the parameter that was passed in.
-function greet(x){
-	return "Hell, "+ x + "!"
+function greet(a) {
+	return "Hello," + a + "!"
 }
-
 
 
 // Q6
@@ -59,9 +65,11 @@ function greet(x){
 // strings. For example:
 // words: "quick", "fox", "fence"
 // sentence: "quick brown fox jumps over the fence"
-//THere were several bricks* laying between * * that * when touched
-function madlib(pluralNoun1, adjective, pluralNoun2, verb1){
-	return "There were several " + pluralNoun1 + "laying between " + adjective + "" + pluralNoun2 + " that " + verb1 + " when touched."
-}
+// Crush your enemies(a), see them driven(b) before you,
+// and hear(c) the lamentation of the women(d)!
 
-madlib()
+
+function madLib (a, b, c, d){
+	//return "Crush your" + a + ", see them" + b + "before you, and" + c + "the lamentation of the" + d + "!"
+	return `Crush your ${a} , see them driven ${b} before you, and ${c} the lamentation of the ${d} !`
+}
